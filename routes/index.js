8bed288b8714d5c5cb3ff8fe024b9c0d3af6a186
salmalah <122815714+salmalah@users.noreply.github.com>
2024-05-11 00:25:09 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import UsersController from '../controllers/UsersController';
 import AppController from '../controllers/AppController';
 
 const router = express.Router();
 
+router.post('/users', UsersController.postNew);
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
