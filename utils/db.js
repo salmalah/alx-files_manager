58d82a7cc0a2f355e8f7492cbe2a5afc9e87d1a1
salmalah <s.salmalahmidi@gmail.com>
2024-05-10 15:53:50 -0700

    MongoDB utils

---
+++ utils/db.js
@@ -0,0 +1,35 @@
+import { MongoClient } from 'mongodb';
+
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || 27017;
+const database = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${host}:${port}`;
+
+class DBClient {
+  constructor() {
+    MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+      if (!err) {
+        this.db = client.db(database);
+        this.users = this.db.collection('users');
+        this.files = this.db.collection('files');
+      } else {
+        console.log(err.message);
+        this.db = false;
+      }
+    });
+  }
+
+  isAlive() { return !!this.db; }
+
+  async nbUsers() { return this.users.countDocuments(); }
+
+  async nbFiles() { return this.files.countDocuments(); }
+
+  async getUser(query) {
+    const user = await this.db.collection('users').findOne(query);
+    return user;
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
